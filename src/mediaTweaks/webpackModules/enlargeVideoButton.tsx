034f6cddc4682d4c805d0f7f4c2d0331bc81c96f
Cynthia Foxwell <gamers@riseup.net>
2024-10-10 18:11:42 -0600

    mediaTweaks: make enlarge video work with imageViewer

--- src/mediaTweaks/webpackModules/enlargeVideoButton.tsx
+++ src/mediaTweaks/webpackModules/enlargeVideoButton.tsx
@@ -2,7 +2,8 @@ import React from "@moonlight-mod/wp/react";
 import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
 import * as Components from "@moonlight-mod/wp/discord/components/common/index";
 
-const { Clickable, Tooltip, MaximizeIcon, openModal, ModalRoot } = Components;
+const { Clickable, Tooltip, MaximizeIcon, openModal, ModalRoot, ModalSize } =
+  Components;
 
 const HoverButtonClasses = spacepack.findByExports("hoverButton")[0].exports;
 
@@ -11,11 +12,12 @@ type HoverButtonsProps = {
   item: Record<string, any>;
 };
 
-// FIXME: will break when IMGXIS gets made, needs remap
-const VideoModal = spacepack.findFunctionByStrings(
-  spacepack.findByCode(".Messages.OPEN_IN_BROWSER")[0].exports,
-  ".videoWrapper"
+const createMediaModal = spacepack.findFunctionByStrings(
+  spacepack.findByCode(".zoomedCarouselModalRoot,items:")[0].exports,
+  '.searchParams.append("format","webp")'
 );
+
+const MediaModalClasses = spacepack.findByExports("modal", "image")[0].exports;
 
 const noop = () => null;
 
@@ -32,23 +34,28 @@ export default function EnlargeVideoButton({
           focusProps={{ offset: 2 }}
           aria-label="Enlarge Video"
           onClick={() => {
-            if (VideoModal != null)
+            if (createMediaModal != null)
               openModal((modalProps: any) => {
+                const { component: MediaModal } = createMediaModal(
+                  {
+                    contentType: item.contentType,
+                    proxyUrl: item.originalItem.proxy_url,
+                    url: item.originalItem.proxy_url,
+                    width: item.originalItem.width,
+                    height: item.originalItem.height
+                  },
+                  noop,
+                  true,
+                  false
+                );
+
                 return (
-                  <ModalRoot {...modalProps} size="dynamic">
-                    {/* @ts-expect-error cope */}
-                    <VideoModal
-                      width={item.originalItem.width}
-                      height={item.originalItem.height}
-                      poster={item.originalItem.proxy_url + "&format=webp"}
-                      naturalWidth={item.originalItem.width}
-                      naturalHeight={item.originalItem.height}
-                      renderLinkComponent={noop}
-                      renderForwardComponent={noop}
-                      src={item.originalItem.proxy_url}
-                      shouldHideMediaOptions={false}
-                      obscure={false}
-                    />
+                  <ModalRoot
+                    {...modalProps}
+                    className={MediaModalClasses.modal}
+                    size={ModalSize.DYNAMIC}
+                  >
+                    {MediaModal}
                   </ModalRoot>
                 );
               });
