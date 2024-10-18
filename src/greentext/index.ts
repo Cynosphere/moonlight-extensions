11947888aa45b15f297218854e4988219a97938b
Cynthia Foxwell <gamers@riseup.net>
2024-10-17 18:40:39 -0600

    greentext+shiftjis: dependencies, dont wait for react

--- src/greentext/index.ts
+++ src/greentext/index.ts
@@ -3,6 +3,6 @@ import { ExtensionWebpackModule } from "@moonlight-mod/types";
 export const webpackModules: Record<string, ExtensionWebpackModule> = {
   greentext: {
     entrypoint: true,
-    dependencies: [{ ext: "markdown", id: "markdown" }, { id: "react" }]
+    dependencies: [{ ext: "markdown", id: "markdown" }]
   }
 };
