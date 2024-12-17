2d37b0787e756ade823cff563292e6eff5852527
Cynthia Foxwell <gamers@riseup.net>
2024-12-16 18:30:01 -0700

    chatTweaks: add no reply chain nag

--- src/chatTweaks/index.ts
+++ src/chatTweaks/index.ts
@@ -1,12 +1,24 @@
-import {Patch} from "@moonlight-mod/types";
+import { Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
-  // reply mention
+  // no reply mention
   {
     find: ',source:"message-actions"})',
     replace: {
       match: /,message:(.),shouldMention:!(.)\.shiftKey&&/,
-      replacement: (_, message, keyEvent) => `,message:${message},shouldMention:moonlight.getConfigOption("chatTweaks","noReplyPing")??true?${keyEvent}.shiftKey:!${keyEvent}.shiftKey&&`
+      replacement: (_, message, keyEvent) =>
+        `,message:${message},shouldMention:moonlight.getConfigOption("chatTweaks","noReplyPing")??true?${keyEvent}.shiftKey:!${keyEvent}.shiftKey&&`
+    }
+  },
+
+  // no reply chain nag
+  {
+    find: ",replyChainLength:",
+    replace: {
+      match: /=(.\.showThreadPromptOnReply)&&/,
+      replacement: (_, showThreadPromptOnReply) =>
+        `=(moonlight.getConfigOption("chatTweaks","noReplyChainNag")??true?false:${showThreadPromptOnReply})&&`
     }
   }
 ];
+
