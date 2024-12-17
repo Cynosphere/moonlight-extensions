ffb5b93fb8666c2dbd6a5beffd12539fe602c0bb
Jane Shimmer <jane@j4.pm>
2024-12-16 19:47:59 -0500

    reviews are in

--- src/chatTweaks/index.ts
+++ src/chatTweaks/index.ts
@@ -3,10 +3,10 @@ import {Patch} from "@moonlight-mod/types";
 export const patches: Patch[] = [
   // reply mention
   {
-    find: /,message:(.),shouldMention:!(.)\.shiftKey&&!.(&&!.)?,/,
+    find: ',source:"message-actions"})',
     replace: {
-      match: /,message:(.),shouldMention:!(.)\.shiftKey&&!.(&&!.)?,/,
-      replacement: (_, orig, keyEvent) => `,message:${orig},shouldMention:moonlight.getConfigOption("chatTweaks","noReplyPing")??true?${keyEvent}.shiftKey:!${keyEvent}.shiftKey,`
+      match: /,message:(.),shouldMention:!(.)\.shiftKey&&/,
+      replacement: (_, message, keyEvent) => `,message:${message},shouldMention:moonlight.getConfigOption("chatTweaks","noReplyPing")??true?${keyEvent}.shiftKey:!${keyEvent}.shiftKey&&`
     }
   }
 ];
