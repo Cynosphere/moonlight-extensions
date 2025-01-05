18aa8e2153818456fc3af6bb4013e6d80e057083
Cynthia Foxwell <gamers@riseup.net>
2025-01-04 22:32:25 -0700

    chatTweaks: add app to bot and webhook tag
    theres settings for no blocked/ignored messages and jump to
    blocked/ignored but not implemented, work check in commit

--- src/chatTweaks/index.ts
+++ src/chatTweaks/index.ts
@@ -1,5 +1,7 @@
 import { Patch } from "@moonlight-mod/types";
 
+let webhookTag = () => moonlight.getConfigOption<boolean>("chatTweaks", "webhookTag") ?? true;
+
 export const patches: Patch[] = [
   // no reply mention
   {
@@ -19,6 +21,34 @@ export const patches: Patch[] = [
       replacement: (_, showThreadPromptOnReply) =>
         `=(moonlight.getConfigOption("chatTweaks","noReplyChainNag")??true?false:${showThreadPromptOnReply})&&`
     }
+  },
+
+  // revert app to bot
+  {
+    find: ".BOT:default:",
+    replace: {
+      match: "9RNkeH", // APP_TAG
+      replacement: "PQt9z8" // BOT_TAG_BOT
+    },
+    prerequisite: () => moonlight.getConfigOption<boolean>("chatTweaks", "appToBot") ?? true
+  },
+
+  // webhook tag
+  {
+    find: ".BOT:default:",
+    replace: {
+      match: /case .\..{1,3}\.BOT:default:(.)=/,
+      replacement: (orig, tag) => `case 99:${tag}="WEBHOOK";break;${orig}`
+    },
+    prerequisite: webhookTag
+  },
+  {
+    find: ".Types.ORIGINAL_POSTER",
+    replace: {
+      match: /(.)=.\.Z\.Types\.ORIGINAL_POSTER\),/,
+      replacement: (orig, type) =>
+        `${orig}(arguments[0].user?.bot&&arguments[0].message?.webhookId&&arguments[0].user?.isNonUserBot?.()&&(${type}=99)),`
+    },
+    prerequisite: webhookTag
   }
 ];
-
