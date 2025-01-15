16b7030f1d9572294ab33776c64cd78870714d11
Cynthia Foxwell <gamers@riseup.net>
2025-01-14 20:13:51 -0700

    dmDates: actually updates now

--- src/dmDates/index.ts
+++ src/dmDates/index.ts
@@ -6,7 +6,9 @@ export const webpackModules: Record<string, ExtensionWebpackModule> = {
     dependencies: [
       { id: "react" },
       { id: "discord/components/common/index" },
+      { id: "discord/packages/flux" },
       { ext: "spacepack", id: "spacepack" },
+      { ext: "common", id: "stores" },
       { ext: "componentEditor", id: "dmList" },
       moonlight.enabledExtensions.has("dmFavorites") && { ext: "dmFavorites", id: "icon" }
     ].filter((d) => !!d)
--- src/dmDates/webpackModules/dmDates.tsx
+++ src/dmDates/webpackModules/dmDates.tsx
@@ -2,13 +2,14 @@ import React from "@moonlight-mod/wp/react";
 import spacepack from "@moonlight-mod/wp/spacepack_spacepack";
 import DMList from "@moonlight-mod/wp/componentEditor_dmList";
 import { Text } from "@moonlight-mod/wp/discord/components/common/index";
+import { useStateFromStores } from "@moonlight-mod/wp/discord/packages/flux";
+import { ReadStateStore } from "@moonlight-mod/wp/common_stores";
 
 const getTimestampString = spacepack.findByCode('["XIGt+f"]')[0].exports;
 const getAbbreviatedFormatter = spacepack.findFunctionByStrings(getTimestampString, '["XIGt+f"]');
-const SnowflakeUtils = spacepack.require("discord/utils/SnowflakeUtils").default;
 
 function DMDate({ channel }: { channel: any }) {
-  const lastMessage = SnowflakeUtils.extractTimestamp(channel.lastMessageId ?? channel.id);
+  const lastMessage = useStateFromStores([ReadStateStore], () => ReadStateStore.lastMessageTimestamp(channel.id));
   const formattedTime = getTimestampString.ZP({ since: lastMessage, getFormatter: getAbbreviatedFormatter });
 
   return (
