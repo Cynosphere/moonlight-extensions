c7e4193b583b87647367c6db2a75d582f52394b2
Cynthia Foxwell <gamers@riseup.net>
2025-02-08 23:42:11 -0700

    betterEmbedsYT: mess with player vars a bit to make the embed player look neater

--- src/betterEmbedsYT/embedBypass.js
+++ src/betterEmbedsYT/embedBypass.js
@@ -1,17 +1,22 @@
-// partially adapted from https://greasyfork.org/en/scripts/465518-youtube-embed-whatever-wherever/code
-// original script doesnt work anymore
+// https://greasyfork.org/en/scripts/465518-youtube-embed-whatever-wherever/code
+// nicer more compact embeds, no youtube logo button
 
 function checkStatus() {
   const ytcfg = window.ytcfg;
   const player_response = JSON.parse(ytcfg.data_.PLAYER_VARS.embedded_player_response);
 
-  if (!player_response?.previewPlayabilityStatus?.status) return;
-  if (
-    player_response.previewPlayabilityStatus.desktopLegacyAgeGateReason ||
-    player_response.previewPlayabilityStatus.status === "OK"
-  )
-    return;
-  location.reload();
+  // watch together doesn't have this field
+  if (player_response.previewPlayabilityStatus) {
+    player_response.previewPlayabilityStatus.status = "OK";
+    player_response.previewPlayabilityStatus.playableInEmbed = true;
+    delete player_response.previewPlayabilityStatus.reason;
+    delete player_response.previewPlayabilityStatus.errorScreen;
+  }
+
+  ytcfg.data_.PLAYER_VARS.embedded_player_response = JSON.stringify(player_response);
+
+  ytcfg.data_.WEB_PLAYER_CONTEXT_CONFIGS.WEB_PLAYER_CONTEXT_CONFIG_ID_EMBEDDED_PLAYER.isEmbed = false;
+  ytcfg.data_.INNERTUBE_CONTEXT.client.originalUrl = `https://www.youtube.com/watch?v=${ytcfg.data_.VIDEO_ID}`;
 }
 
 function waitForConfig() {
