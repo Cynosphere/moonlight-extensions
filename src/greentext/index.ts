066492dc5c216721b972005f225cbaf13b1ebbff
Cynthia Foxwell <gamers@riseup.net>
2025-05-09 21:07:31 -0600

    greentext: add React to dependencies so no crash if React is missing

--- src/greentext/index.ts
+++ src/greentext/index.ts
@@ -3,6 +3,6 @@ import { ExtensionWebpackModule } from "@moonlight-mod/types";
 export const webpackModules: Record<string, ExtensionWebpackModule> = {
   greentext: {
     entrypoint: true,
-    dependencies: [{ ext: "markdown", id: "markdown" }]
+    dependencies: [{ id: "react" }, { ext: "markdown", id: "markdown" }]
   }
 };
