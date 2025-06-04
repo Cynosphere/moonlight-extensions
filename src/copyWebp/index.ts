d8afa3a43ae84f895446406d46b348d295c6ddad
Cynthia Foxwell <gamers@riseup.net>
2025-06-04 11:35:27 -0600

    copyWebp: fix

--- src/copyWebp/index.ts
+++ src/copyWebp/index.ts
@@ -1,25 +1,24 @@
 import { Patch } from "@moonlight-mod/types";
 
 export const patches: Patch[] = [
-  // add to copy list
   {
     find: "discord/utils/NativeUtils",
-    replace: {
-      match: '"png"]',
-      replacement: '"png","webp"]'
-    }
-  },
-
-  // intercept copying to convert if needed
-  {
-    find: '"Copy image method called outside native app"',
-    replace: {
-      match: /(\i)=await \i\(\i\);/,
-      replacement: (orig, buffer) => `${orig}
+    replace: [
+      // add to copy list
+      {
+        match: '"png"]',
+        replacement: '"png","webp"]'
+      },
+      // intercept copying to convert if needed
+      {
+        match: /(?<=;)\i\.clipboard\.copyImage\(\i\.from\((\i)\),\i\)/,
+        replacement: (orig, buffer) => `
 if(/RIFF...?.?WEBP/.test(new TextDecoder("utf-8").decode(${buffer}.slice(0,12)))){
   ${buffer} = await moonlight.getNatives("copyWebp").convertWebp(${buffer});
 }
+${orig}
 `
-    }
+      }
+    ]
   }
 ];
