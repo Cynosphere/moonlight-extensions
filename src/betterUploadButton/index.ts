af07a8d30484021036a6ce59b4658977c12e0c25
Cynthia Foxwell <gamers@riseup.net>
2025-08-07 11:06:50 -0600

    betterUploadButton: fix patch

--- src/betterUploadButton/index.ts
+++ src/betterUploadButton/index.ts
@@ -4,10 +4,16 @@ export const patches: Patch[] = [
   {
     find: '("ChannelAttachButton")',
     replace: {
-      match:
-        /(?<="Invalid popout type provided"\)}},children:\i=>\(0,\i\.jsx\)\(\i\.\i,)(\i)\((\i)\({(buttonRef|look):/,
+      match: /(?<="Invalid popout type provided"\)}},children:\i=>\(0,\i\.jsx\)\(\i\.\i,)(\i)\((\i)\({(look|ref):/,
       replacement: (_, defineProps, mergeProps, firstProp) =>
         `require("betterUploadButton_fixProps").default(${mergeProps}({${firstProp}:`
+    }
+  },
+  {
+    find: ".CHAT_INPUT_BUTTON_NOTIFICATION,width:",
+    replace: {
+      match: /,onClick:(\i)\?void 0:/,
+      replacement: (orig, disabled) => `,onContextMenu:${disabled}?void 0:arguments[0].onContextMenu${orig}`
     }
   }
 ];
