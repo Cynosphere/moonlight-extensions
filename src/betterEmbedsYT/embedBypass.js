7ddee54aca1d71a24278a5dd71ad1f3dbb308739
Cynthia Foxwell <gamers@riseup.net>
2026-03-31 17:02:42 -0600

    betterEmbedsYT: player fixes

--- src/betterEmbedsYT/embedBypass.js
+++ src/betterEmbedsYT/embedBypass.js
@@ -1,9 +1,10 @@
 // https://greasyfork.org/en/scripts/465518-youtube-embed-whatever-wherever/code
-// nicer more compact embeds, no youtube logo button
+// ~~nicer more compact embeds, no youtube logo button~~
+// native browser player since the new youtube ui broke the old player completely
 
 function checkStatus() {
-  const ytcfg = window.ytcfg;
-  const player_response = JSON.parse(ytcfg.data_.PLAYER_VARS.embedded_player_response);
+  const ytcfg = window.yt?.config_ ?? window.ytcfg?.data_;
+  const player_response = JSON.parse(ytcfg.PLAYER_VARS.embedded_player_response);
 
   // watch together doesn't have this field
   if (player_response.previewPlayabilityStatus) {
@@ -13,21 +14,24 @@ function checkStatus() {
     delete player_response.previewPlayabilityStatus.errorScreen;
   }
 
-  ytcfg.data_.PLAYER_VARS.embedded_player_response = JSON.stringify(player_response);
+  ytcfg.PLAYER_VARS.embedded_player_response = JSON.stringify(player_response);
 
-  ytcfg.data_.WEB_PLAYER_CONTEXT_CONFIGS.WEB_PLAYER_CONTEXT_CONFIG_ID_EMBEDDED_PLAYER.isEmbed = false;
-  ytcfg.data_.INNERTUBE_CONTEXT.client.originalUrl = `https://www.youtube.com/watch?v=${ytcfg.data_.VIDEO_ID}`;
+  ytcfg.WEB_PLAYER_CONTEXT_CONFIGS.WEB_PLAYER_CONTEXT_CONFIG_ID_EMBEDDED_PLAYER.isEmbed = false;
+  ytcfg.WEB_PLAYER_CONTEXT_CONFIGS.WEB_PLAYER_CONTEXT_CONFIG_ID_EMBEDDED_PLAYER.disableOrganicUi = false;
+  ytcfg.WEB_PLAYER_CONTEXT_CONFIGS.WEB_PLAYER_CONTEXT_CONFIG_ID_EMBEDDED_PLAYER.playerStyle = "play";
+  ytcfg.WEB_PLAYER_CONTEXT_CONFIGS.WEB_PLAYER_CONTEXT_CONFIG_ID_EMBEDDED_PLAYER.useNativeControls = true;
+  ytcfg.INNERTUBE_CONTEXT.client.originalUrl = `https://www.youtube.com/watch?v=${ytcfg.VIDEO_ID}`;
 }
 
 function waitForConfig() {
-  if (window.ytcfg) {
+  if (window.yt?.config_ ?? window.ytcfg?.data_) {
     checkStatus();
   } else {
     requestAnimationFrame(waitForConfig);
   }
 }
 
-if (window.ytcfg) {
+if (window.yt?.config_ ?? window.ytcfg?.data_) {
   checkStatus();
 } else {
   waitForConfig();
